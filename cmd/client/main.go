4c33711939142c637adbd3fb762fc236730011a2
T2Knock <huynhtransongngoc@gmail.com>
2026-02-27 21:35:14 +0700

    feat: add graceful shutdown on client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/rabbitmq"
+)
+
+type config struct {
+	addr string
+}
 
 func main() {
-	fmt.Println("Starting Peril client...")
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	if err := run(ctx, os.Args, os.Stdout); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+
+	log.Println("shutdown complete")
+	os.Exit(0)
+}
+
+func run(ctx context.Context, args []string, stdout io.Writer) error {
+	var cfg config
+
+	flags := flag.NewFlagSet(args[0], flag.ContinueOnError)
+	flags.SetOutput(stdout)
+
+	flags.StringVar(&cfg.addr, "addr", "amqp://guest:guest@localhost:5672", "addr of RabbitMQ amqp")
+
+	if err := flags.Parse(args[1:]); err != nil {
+		return err
+	}
+
+	client, err := rabbitmq.NewClient(cfg.addr)
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+
+	gamelogic.ClientWelcome()
+
+	<-ctx.Done()
+
+	return nil
 }
